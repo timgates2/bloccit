75c87c0ab53968f841ffe62f5596dad0f8cd7031
Tim Gates <timgates2@gmail.com>
2015-10-12 15:41:34 +0000

    Completed Checkpoint 29

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe WelcomeController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #about" do
-    it "returns http success" do
-      get :about
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+RSpec.describe WelcomeController, :type => :controller do
+   describe "GET index" do
+     it "renders the index template" do
+ # #2
+       get :index
+ # #3
+       expect(response).to render_template("index")
+     end
+   end
+ 
+   describe "GET about" do
+     it "renders the about template" do
+       get :about
+       expect(response).to render_template("about")
+     end
+   end  
 end
