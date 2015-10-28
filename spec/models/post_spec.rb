764127381a883f88700ac6e8070f59b3e28c8c93
Tim Gates <timgates2@gmail.com>
2015-10-28 16:26:54 +0000

    Checkpoint 35 Rails Validation completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,7 +7,14 @@ RSpec.describe Post, type: :model do
    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
    let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
  
-   it { should belong_to(:topic) }   
+   it { should belong_to(:topic) }  
+ # #1
+   it { should validate_presence_of(:title) }
+   it { should validate_presence_of(:body) }
+   it { should validate_presence_of(:topic) }
+ # 2
+   it { should validate_length_of(:title).is_at_least(5) }
+   it { should validate_length_of(:body).is_at_least(20) }
    
   describe "attributes" do
       it "should respond to title" do
@@ -19,3 +26,4 @@ RSpec.describe Post, type: :model do
       end
   end
 end
+
