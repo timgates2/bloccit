f94c12ec5936bd03eca679cdcda39c1c964151c3
Tim <timgates2@gmail.com>
2015-11-14 11:06:54 -0600

    Complete assignment-41

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe Comment, type: :model do
 
     it { should belong_to(:post) }
     it { should belong_to(:user) }
+
     it { should validate_presence_of(:body) }
     it { should validate_length_of(:body).is_at_least(5) }
 
